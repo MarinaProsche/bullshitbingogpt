9cbd93e9afa864461fe06c934128894ceb1bbee3
Marina <verika_iz_viro@mail.ru>
2023-08-20 15:22:10 +0300

    first vision

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder= 'templates')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['post', 'get'])
+def text():
+    if request.method == "GET":
+        text = request.form.get('text')
+        return render_template('text.html', text=text)
+    if request.method == "POST":
+        text = 'дождитесь генерации'
+        return render_template('after_text.html', text=text)
 
-# if __name__ == '__main__': 
-#    app.run(host='0.0.0.0', port=81)
+# @app.route('/text/', methods=['post', 'get'])
+# def false():
+#     message = ''
+#     if request.method == 'POST':
+#         text = request.form.get('text')
+#     if text:
+#         message = 'Please wait'
+#     return render_template('text.html', message=message)

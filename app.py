547a8f96b679fc401a48383c35c77e782f4a993c
Marina <verika_iz_viro@mail.ru>
2023-08-31 14:11:24 +0300

    full house of pages

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
 import openai
-from chatgpt import find_theme_for_sent_text, find_general_theme_for_sent_text, match_buzzwords
+from chatgpt import (
+    find_theme_for_sent_text,
+    find_general_theme_for_sent_text,
+    match_buzzwords,
+    get_result_message,
+    get_text_about,
+)
 from generator_for_themes import get_theme_architecture, get_buzzwords_for_theme
 
 from dataclasses import dataclass
+
 
 @dataclass
 class BuzzwordsMatch:
@@ -11,26 +18,54 @@ class BuzzwordsMatch:
     buzzword: str
     match: bool
 
-app = Flask(__name__, template_folder= 'templates')
 
-@app.route("/", methods=['post', 'get'])
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route("/", methods=["post", "get"])
 def text():
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template("index.html")
     if request.method == "POST":
-        text = request.form.get('input_text')
+        text = request.form.get("input_text")
         themes = get_theme_architecture()
         general_theme = find_general_theme_for_sent_text(themes=list(themes), text=text)
         if general_theme in themes:
             list_of_under_themes = themes[general_theme]
-            theme_of_sent_text = find_theme_for_sent_text(text=text, under_themes=list_of_under_themes)
+            theme_of_sent_text = find_theme_for_sent_text(
+                text=text, under_themes=list_of_under_themes
+            )
             if theme_of_sent_text in themes[general_theme]:
                 list_final_buzzwords = get_buzzwords_for_theme(theme_of_sent_text)
-                buzzword_match = match_buzzwords(text=text, buzzwords=list_final_buzzwords)
-                buzz_m_list = [BuzzwordsMatch(buzzword=buzzword, match=buzzword.lower() in buzzword_match.lower()) for buzzword in list_final_buzzwords]
-                buzz_m_list = buzz_m_list[:12] + [BuzzwordsMatch(buzzword='', match=True)] + buzz_m_list[12:]   
-                return render_template('bingo.html', buzz_m_list=buzz_m_list, theme_of_sent_text=theme_of_sent_text, buzzword_match=buzzword_match)
+                buzzword_match = match_buzzwords(
+                    text=text, buzzwords=list_final_buzzwords
+                )
+                buzz_m_list = [
+                    BuzzwordsMatch(
+                        buzzword=buzzword,
+                        match=buzzword.lower() in buzzword_match.lower(),
+                    )
+                    for buzzword in list_final_buzzwords
+                ]
+                buzz_m_list = (
+                    buzz_m_list[:12]
+                    + [BuzzwordsMatch(buzzword="", match=True)]
+                    + buzz_m_list[12:]
+                )
+                result_message = get_result_message(
+                    theme=theme_of_sent_text,
+                    score=len([x for x in buzz_m_list if x.match]),
+                )
+                return render_template(
+                    "bingo.html", buzz_m_list=buzz_m_list, result_message=result_message
+                )
             else:
-                return render_template('index.html', chat_response=theme_of_sent_text)
+                return render_template("index.html", chat_response=theme_of_sent_text)
         else:
-            return render_template('index.html', chat_response=general_theme)
+            return render_template("index.html", chat_response=general_theme)
+
+
+@app.route("/about", methods=["GET"])
+def text_about():
+    text_about = get_text_about()
+    return render_template("about.html", text_about=text_about)
--- chatgpt.py
+++ chatgpt.py
@@ -1,25 +1,31 @@
-
 import openai
 
 TEXT_ABOUT_LANGUAGE = "\n\n\n give the answer in the same language as the text"
 
+
 def openai_key():
-    with open('.openaikey', 'r') as f:
+    with open(".openaikey", "r") as f:
         key = f.read().strip()
         return key
 
+
 openai.api_key = openai_key()
-                
+
 
 def generate_buzzwords_for_theme(theme):
-    prompt = ("create 24 buzzwords that could be used for bullshit bingo for this theme: \n\n\n"
-    + theme)
+    prompt = (
+        "create 24 buzzwords that could be used for bullshit bingo for this theme: \n\n\n"
+        + theme
+    )
     buzzwords = chat_with_chatgpt(prompt=prompt)
     return buzzwords
 
+
 def find_general_theme_for_sent_text(themes, text):
-    themes_str = '\n'.join(themes)
-    prompt = f"this is the text \n\n{text}.\n\nWhat kind of theme is it? \n\n{themes_str}"
+    themes_str = "\n".join(themes)
+    prompt = (
+        f"this is the text \n\n{text}.\n\nWhat kind of theme is it? \n\n{themes_str}"
+    )
     print(prompt)
     general_theme = chat_with_chatgpt(prompt=prompt)
     for theme in themes:
@@ -27,8 +33,9 @@ def find_general_theme_for_sent_text(themes, text):
             return theme
     return general_theme
 
+
 def find_theme_for_sent_text(under_themes, text):
-    under_themes_str = '\n'.join(under_themes)
+    under_themes_str = "\n".join(under_themes)
     prompt = f"this is the list of themes:\n\n{under_themes_str}\n\n\nfind the one, that describes the following text the best and print only the theme:\n\n{text}"
     theme_of_text = chat_with_chatgpt(prompt=prompt)
     for theme in under_themes:
@@ -36,19 +43,32 @@ def find_theme_for_sent_text(under_themes, text):
             return theme
     return theme_of_text
 
+
 def match_buzzwords(text, buzzwords):
     buzzwords_str = str(buzzwords)
     prompt = f"select from this list: \n\n{buzzwords_str}\n\n all items that could describe this text\n\n{text}"
-    print(prompt)
+    return chat_with_chatgpt(prompt=prompt)
+
+
+def get_result_message(theme, score):
+    prompt = (
+        f"Generate message that congratulates a player who sent text submissions got {score}"
+        + f' out of 24 in "Bullshit Bingo GPT" game that automatically matches the buzzwords for theme "{theme}". Use emojis.'
+    )
+    return chat_with_chatgpt(prompt=prompt)
+
+def get_text_about():
+    prompt = (f'Please write description for the website "Bullshit Bingo GPT" where you can provide an input ' +
+        f'text and it automatically generates bullshit bingo card for this text by exposing buzzwords, cliches from the text. ' + 
+        f'We are using power of modern AI, you should mention it. Use as much bullshit in your answer. Use a lot of emojis. ' +
+        f"Note that we don't store users text on the server, so it's confidential.")
     return chat_with_chatgpt(prompt=prompt)
 
 def chat_with_chatgpt(prompt, model="gpt-3.5-turbo-16k"):
     response = openai.ChatCompletion.create(
         # engine=model,
         model=model,
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
+        messages=[{"role": "user", "content": prompt}],
         # max_tokens=100,
         # n=1,
         # stop=None,
